93e2e3b6986b58e6b3db54a0492d0784eb76b95c
Michael Hampton <hamptonm88@gmail.com>
2021-06-19 15:52:46 -0700

    Add item name to hover box

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# mura-env vs code extension
+## Mura Configuration Helper Visual Studio Code extension
 
 This extension is to easily see what MURA_ environment variables do as per Mura's documentation found at https://docs.murasoftware.com/v10/extending-mura/mura-file-structure/#section-the-settingsinicfm-file
--- src/extension.js
+++ src/extension.js
@@ -1,37 +1,39 @@
 const vscode = require('vscode');
 
-const { map } = require('./items')
-console.log(map)
-
+const { map } = require('./items');
+console.log(map);
 
 /**
  * @param {string} word
  */
 function getInfo(word) {
-	word = word.toUpperCase()
-	return new vscode.Hover(`**Type**: ${map[word].type}\n\n**Default**: ${map[word].default}\n\n**Description**: ${map[word].description}`)
+	word = word.toUpperCase();
+	return new vscode.Hover(`## ${word}\n\n**Type**: ${map[word].type}\n\n**Default**: ${map[word].default}\n\n**Description**: ${map[word].description}`);
 }
 
+/**
+ * @param {{ subscriptions: vscode.Disposable[]; }} context
+ */
 function activate(context) {
 
 	let ymlDisposable = vscode.languages.registerHoverProvider('yaml', {
-		provideHover(document, position, token) {
+		provideHover(document, position) {
 			const wordRange = document.getWordRangeAtPosition(position);
 			const word = document.getText(wordRange);
 
 			if (word.includes("MURA_")) {
-				let splitWord = word.split('_')
-				return getInfo(splitWord[1])
+				let splitWord = word.split('_');
+				return getInfo(splitWord[1]);
 			}
 		}
 	});
 
 	let iniDisposable = vscode.languages.registerHoverProvider('ini', {
-		provideHover(document, position, token) {
+		provideHover(document, position) {
 			const wordRange = document.getWordRangeAtPosition(position);
 			const word = document.getText(wordRange);
 			
-			return getInfo(word)
+			return getInfo(word);
 		}
 	});
 
@@ -43,4 +45,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-}
+};
